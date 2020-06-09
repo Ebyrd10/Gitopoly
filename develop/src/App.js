4b5cad6e32cb07efc79e0c56fa0621d5286e3f9a
Ethan <Ethanmbyrd@gmail.com>
2020-06-09 13:48:07 -0400

    Adding basic property cards and board layout

--- develop/src/App.js
+++ develop/src/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import Dice from './components/Dice/index';
-
+import React from "react";
+import Dice from "./components/Dice/index";
+import Property from "./components/Property";
+import './style.css'
 
 // Calculator component
 function App() {
-  
   return (
     <>
-    <Dice/>
+      <div id="gameBoard">
+        <Property />
+        <Property />
+        <Property />
+        <Property />
+        <Property />
+        <Property />
+        <Property />
+        <Property />
+        <Property />
+        <Property />
+        <Property />
+        <Property />
+        <Property />
+        <Property /> 
+        <div id="centerItem" > <Dice /> </div>
+      </div>
     </>
-  )
-
+  );
 }
 
 export default App;
