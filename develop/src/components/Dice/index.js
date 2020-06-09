e6dad3df66e178117885c25d6f8e96597f51829a
Ethan <Ethanmbyrd@gmail.com>
2020-06-09 18:21:11 -0400

    adding dice sound effect

--- develop/src/components/Dice/index.js
+++ develop/src/components/Dice/index.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import './style.css';
+import React, { Component } from "react";
+import "./style.css";
+import soundEffect from "./dice.wav"
 
 export class Dice extends Component {
   rollDice = () => {
-    const die1 = document.getElementById('die1');
-    const die2 = document.getElementById('die2');
-    const status = document.getElementById('status');
+    const die1 = document.getElementById("die1");
+    const die2 = document.getElementById("die2");
+    const status = document.getElementById("status");
     const d1 = Math.floor(Math.random() * 6) + 1;
     const d2 = Math.floor(Math.random() * 6) + 1;
     const diceTotal = d1 + d2;
@@ -16,26 +17,35 @@ export class Dice extends Component {
     if (d1 == d2) {
       // status.innerHTML += ' DOUBLES! You get a free turn!!';
       // status.innerHTML += ' Doubles, Free Turn';
-      status.innerHTML += '\n <span>D</span><span>o</span><span>u</span><span>b</span><span>l</span><span>e</span><span>s</span>';
+      status.innerHTML +=
+        "\n <span>D</span><span>o</span><span>u</span><span>b</span><span>l</span><span>e</span><span>s</span>";
     }
+    this.playSound(soundEffect);
   };
+
+  playSound = (src) => {
+    const sound = document.createElement("audio");
+    sound.src = src;
+    document.querySelector('#diceContainer').appendChild(sound);
+    sound.play();
+  }
 
   render() {
     return (
       <div>
-      <div id="diceContainer">
-        <div id="numberSlotsContainer">
-        <div id="die1" className="dice">
-          0
-        </div>
-        <div id="die2" className="dice">
-          0
-        </div>
-        </div>
-        <div id="dieBtn" onClick={this.rollDice}>
-          <i class="fas fa-dice-six fa-6x"></i>
+        <div id="diceContainer">
+          <div id="numberSlotsContainer">
+            <div id="die1" className="dice">
+              0
+            </div>
+            <div id="die2" className="dice">
+              0
+            </div>
           </div>
-        {/* <br></br>
+          <div id="dieBtn" onClick={this.rollDice}>
+            <i className="fas fa-dice-six fa-6x"></i>
+          </div>
+          {/* <br></br>
         <hr></hr>
         <div>
           <ul>
@@ -44,8 +54,8 @@ export class Dice extends Component {
             </li>
           </ul>
         </div> */}
-        <h2 id="status"></h2>
-      </div>
+          <h2 id="status"></h2>
+        </div>
       </div>
     );
   }
