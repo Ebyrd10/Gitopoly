89efc7b1e9295aca293b84d24f35684495d82032
Ethan <Ethanmbyrd@gmail.com>
2020-06-10 10:42:39 -0400

    adding prop values

--- develop/src/components/Property/index.js
+++ develop/src/components/Property/index.js
@@ -5,17 +5,16 @@ export class PropertyCard extends Component {
   render() {
     return (
       <div className="card">
-        <div className="cardHeader" style={{backgroundColor: 'red'}}></div>
+        <div className="cardHeader" style={{backgroundColor: this.props.group ? this.props.group: 'red'}}></div>
         <div className="titleContainer">
           <div className="title"> Title Deed</div>
-          <div className="subTitle"> Boardwalk</div>
+          <div className="subTitle"> {this.props.name ? this.props.name : 'boardwalk'}</div>
         </div>
         <div className="cardBody">
             <ul className ="bodyList">
-                <li> Value: $100 </li>
-                <li>Owned By: </li>
-                {/* <li>Group: Red</li> */}
-                <li>Mortaged: No</li>
+                <li> Value: ${this.props.value ? this.props.value : 100} </li>
+                <li>Owned By:{this.props.ownedBy ? this.props.ownedBy : ''}</li>
+                <li>Mortaged:{this.props.mortaged ? 'Yes' : 'No'}</li>
             </ul>
         </div>
       </div>
