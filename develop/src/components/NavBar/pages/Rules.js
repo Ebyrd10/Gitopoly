5e3d560f80e93f253b26af46a55dcb0f118a44f6
SungJin Kim <sungjinkimm@gmail.com>
2020-06-14 16:30:35 -0400

    updated contents in the navbar

--- develop/src/components/NavBar/pages/Rules.js
+++ develop/src/components/NavBar/pages/Rules.js
@@ -8,7 +8,6 @@ function Rules () {
       <div>
         <p><span id="list-tag"> 1. </span> <a href="https://howdoyouplayit.com/monopoly-rules-play-monopoly/#:~:text=Each%20player%20chooses%20a%20token,from%20the%20Banker's%20own%20assets." target="_blank" rel="noopener noreferrer"> Monopoly Rules</a></p>
         <p><span id="list-tag"> 2. </span> <a href="https://en.wikibooks.org/wiki/Monopoly/Official_Rules" target="_blank" rel="noopener noreferrer"> Wiki Rules </a></p>
-        <p><span id="list-tag"> 3. </span> ipsum!</p>
       </div>
     </div>
   );
